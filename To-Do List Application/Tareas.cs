87c10747bc5849d3f6783a5c3bd75bfc37d3fdc3
flor <fflorbruno@gmail.com>
2023-11-08 19:30:22 -0300

    Clase tarea creada

---
+++ To-Do List Application/Tareas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_Do_List_Application
+{
+    class Tareas
+    {
+        private string nombre;
+        private string descripcion;
+        private DateTime fechaCreacion, fechaEvento;
+        public bool terminado = false;
+
+        public string Nombre { get { return nombre; } }
+        public string Descripcion { get { return descripcion; } }
+        public DateTime FechaCreacion { get { return fechaCreacion; } }
+        public DateTime FechaEvento { get { return fechaEvento; } set { fechaEvento = value; } }
+
+        public Tareas (string n, string d)
+        {
+            nombre = n;
+            descripcion = d;
+            fechaCreacion = DateTime.Now;
+        }
+
+    }
+
+
+}
+}
+    
